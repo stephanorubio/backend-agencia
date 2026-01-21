7eacbe1c24e66cd4663675f68cb734d27620f09c
stephanorubio <87827011+stephanorubio@users.noreply.github.com>
2026-01-20 22:40:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 3000;
 const SECRET_KEY = process.env.JWT_SECRET;
 
 app.use(cors());
-app.use(express.json());
+// AUMENTAMOS EL LÍMITE DE CARGA A 50MB PARA LAS IMÁGENES
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- MIDDLEWARE DE SEGURIDAD ---
 const verifyToken = (req, res, next) => {
